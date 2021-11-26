180f6a795e7459d3edcfdbb68679de39b420a92e
Allana Karla <allanakarla1702@gmail.com>
2021-11-25 22:31:55 -0300

    Requisitos 7 e 8

--- script.js
+++ script.js
@@ -3,9 +3,22 @@ buttonTag.addEventListener("click", addValue);
 
 function addValue() {
     let textValue = document.querySelector("#texto-tarefa").value;
-    let listTag = document.createElement("li");
+    let listTag = document.createElement("li")
     listTag.innerHTML = textValue;
     document.getElementById("lista-tarefas").appendChild(listTag);
    //Remove/exclui "value" do input(barra de digitação)
-    document.getElementById("texto-tarefa").value = "";   
+    document.getElementById("texto-tarefa").value = "";  
+    listTag.addEventListener("click", colorValue);
+ }
+
+ function colorValue(event) {
+    let elementGray = document.querySelector(".grey");
+    
+    if (elementGray) {
+        elementGray.classList.remove("grey");  
+    }
+    event.target.classList.add("grey");
+   
 }
+
+
